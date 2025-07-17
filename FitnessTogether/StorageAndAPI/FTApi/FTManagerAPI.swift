9c0e7c95df3647aa7ea015f8fca6fe187626e7d1
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2025-07-17 12:20:44 +0300

    написал адаптер для апишки

---
+++ FitnessTogether/StorageAndAPI/FTApi/FTManagerAPI.swift
@@ -0,0 +1,149 @@
+
+import FTApi
+import Foundation
+
+public final class FTManagerAPI: FTManager {
+
+    public var user: any FTUserInterface
+    public var workout: any FTWorkoutInterface
+    public var exercise: any FTExerciseInterface
+    public var set: any FTSetInterface
+    
+    public init() {
+        let api = FTApi()
+        user = FTUserApiAdapter(userAPI: api.user)
+        workout = FTWorkoutApiAdapter(workoutAPI: api.workout)
+        exercise = FTExerciseApiAdapter(exerciseAPI: api.exercise)
+        set = FTSetApiAdapter(setAPI: api.set)
+    }
+    
+}
+
+fileprivate final class FTUserApiAdapter: FTUserInterface {
+    var userAPI: FTUserManager
+    var token: String? { userAPI.token }
+    var hasPreviousLogin: Bool { userAPI.hasPreviousLogin }
+    
+    init(userAPI: FTUserManager) {
+        self.userAPI = userAPI
+    }
+    
+    func register(data: FTUserRegister, completion: FTCompletion<Data>) {
+        userAPI.register(data: data, completion: completion)
+    }
+    
+    func login(data: FTUserLogin, completion: FTCompletion<Data>) {
+        userAPI.login(data: data, completion: completion)
+    }
+    
+    func loginWithPreviousCredentials(completion: FTCompletion<Data>) {
+        userAPI.loginWithPreviousCredentials(completion: completion)
+    }
+    
+    func logout(completion: FTCompletion<Void>) {
+        userAPI.logout(completion: completion)
+    }
+    
+    func current(completion: FTCompletion<FTUser>) {
+        userAPI.current(completion: completion)
+    }
+    
+    func addClientToCoach(clientId: String, completion: FTCompletion<Void>) {
+        userAPI.addClientToCoach(clientId: clientId, completion: completion)
+    }
+    
+    func getClients(completion: FTCompletion<[FTUser]>) {
+        userAPI.getClients(completion: completion)
+    }
+    
+}
+
+fileprivate final class FTWorkoutApiAdapter: FTWorkoutInterface {
+    
+    var workoutAPI: FTWorkoutManager
+    
+    init(workoutAPI: FTWorkoutManager) {
+        self.workoutAPI = workoutAPI
+    }
+
+    func create(data: FTWorkoutCreate, completion: FTCompletion<FTWorkout>) {
+        workoutAPI.create(data: data, completion: completion)
+    }
+    
+    func get(workoutId: String, completion: FTCompletion<FTWorkout>) {
+        workoutAPI.get(workoutId: workoutId, completion: completion)
+    }
+    
+    func getAll(completion: FTCompletion<[FTWorkout]>) {
+        workoutAPI.getAll(completion: completion)
+    }
+    
+    func edit(workoutId: String, newData data: FTWorkoutCreate, completion: FTCompletion<FTWorkout>) {
+        workoutAPI.edit(workoutId: workoutId, newData: data, completion: completion)
+    }
+    
+    func delete(workoutId: String, completion: FTCompletion<Void>) {
+        workoutAPI.delete(workoutId: workoutId, completion: completion)
+    }
+    
+}
+
+fileprivate final class FTExerciseApiAdapter: FTExerciseInterface {
+    
+    var exerciseAPI: FTExerciseManager
+    
+    init(exerciseAPI: FTExerciseManager) {
+        self.exerciseAPI = exerciseAPI
+    }
+    
+    func create(data: FTExerciseCreate, completion: FTCompletion<FTExercise>) {
+        exerciseAPI.create(data: data, completion: completion)
+    }
+    
+    func get(exerciseId: String, completion: FTCompletion<FTExercise>) {
+        exerciseAPI.get(exerciseId: exerciseId, completion: completion)
+    }
+    
+    func get(workoutId: String, completion: FTCompletion<[FTExercise]>) {
+        exerciseAPI.get(workoutId: workoutId, completion: completion)
+    }
+    
+    func update(exerciseId: String, data: FTExerciseCreate, completion: FTCompletion<FTExercise>) {
+        exerciseAPI.update(exerciseId: exerciseId, data: data, completion: completion)
+    }
+    
+    func delete(exerciseId: String, completion: FTCompletion<Void>) {
+        exerciseAPI.delete(exerciseId: exerciseId, completion: completion)
+    }
+    
+}
+
+fileprivate final class FTSetApiAdapter: FTSetInterface {
+    
+    var setAPI: FTSetManager
+    
+    init(setAPI: FTSetManager) {
+        self.setAPI = setAPI
+    }
+    
+    func create(data: FTSetCreate, completion: FTCompletion<FTSet>) {
+        setAPI.create(data: data, completion: completion)
+    }
+    
+    func get(setId: String, completion: FTCompletion<FTSet>) {
+        setAPI.get(setId: setId, completion: completion)
+    }
+    
+    func get(exerciseId: String, completion: FTCompletion<[FTSet]>) {
+        setAPI.get(exerciseId: exerciseId, completion: completion)
+    }
+    
+    func edit(setId: String, newData data: FTSetCreate, completion: FTCompletion<FTSet>) {
+        setAPI.edit(setId: setId, newData: data, completion: completion)
+    }
+    
+    func delete(setId: String, completion: FTCompletion<Void>) {
+        setAPI.delete(setId: setId, completion: completion)
+    }
+    
+}
---
+++ FitnessTogether/StorageAndAPI/FTManager.swift
@@ -0,0 +1,47 @@
+
+import Foundation
+import FTApi
+
+public protocol FTManager {
+    var user: FTUserInterface { get }
+    var workout: FTWorkoutInterface { get }
+    var exercise: FTExerciseInterface { get }
+    var set: FTSetInterface { get }
+}
+
+public protocol FTUserInterface {
+    var token: String? { get }
+    var hasPreviousLogin: Bool { get }
+    
+    func register(data: FTUserRegister, completion: FTCompletion<Data>)
+    func login(data: FTUserLogin, completion: FTCompletion<Data>)
+    func loginWithPreviousCredentials(completion: FTCompletion<Data>)
+    func logout(completion: FTCompletion<Void>)
+    func current(completion: FTCompletion<FTUser>)
+    func addClientToCoach(clientId: String, completion: FTCompletion<Void>)
+    func getClients(completion: FTCompletion<[FTUser]>)
+}
+
+public protocol FTWorkoutInterface {
+    func create(data: FTWorkoutCreate, completion: FTCompletion<FTWorkout>)
+    func get(workoutId: String, completion: FTCompletion<FTWorkout>)
+    func getAll(completion: FTCompletion<[FTWorkout]>)
+    func edit(workoutId: String,newData data: FTWorkoutCreate, completion: FTCompletion<FTWorkout>)
+    func delete(workoutId: String, completion: FTCompletion<Void>)
+}
+
+public protocol FTExerciseInterface {
+    func create(data: FTExerciseCreate, completion: FTCompletion<FTExercise>)
+    func get(exerciseId: String, completion: FTCompletion<FTExercise>)
+    func get(workoutId: String, completion: FTCompletion<[FTExercise]>)
+    func update(exerciseId: String, data: FTExerciseCreate, completion: FTCompletion<FTExercise>)
+    func delete(exerciseId: String, completion: FTCompletion<Void>)
+}
+
+public protocol FTSetInterface {
+    func create(data: FTSetCreate, completion: FTCompletion<FTSet>)
+    func get(setId: String, completion: FTCompletion<FTSet>)
+    func get(exerciseId: String, completion: FTCompletion<[FTSet]>)
+    func edit(setId: String, newData data: FTSetCreate, completion: FTCompletion<FTSet>)
+    func delete(setId: String, completion: FTCompletion<Void>)
+}
