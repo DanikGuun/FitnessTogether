70d451a5cbf596e8ac92674db9a26a5ae28e687d
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2025-07-21 20:52:44 +0300

    вынес моки в тестах

---
+++ FitnessTogetherTests/RegistrationStates/MockRegistrationDelegate.swift
@@ -0,0 +1,29 @@
+
+import UIKit
+import XCTest
+@testable import FitnessTogether
+
+internal class MockRegistrationDelegate: RegistrationStateDelegate {
+    
+    var goNextCalled = false
+    var lastAfterView: UIView?
+    var lastViewInserted: UIView?
+    var lastViewRemoved: UIView?
+    
+    func registrationStateGoNext(_ state: RegistrationState) {
+        goNextCalled = true
+    }
+    
+    func registrationState(_ state: RegistrationState, needInertView view: UIView, after afterView: UIView) {
+        lastAfterView = afterView
+        lastViewInserted = view
+    }
+    
+    func registrationState(_ state: RegistrationState, needReplaceView view: UIView, with otherView: UIView) {
+        
+    }
+    
+    func registrationState(_ state: RegistrationState, needRemoveView view: UIView) {
+        lastViewRemoved = view
+    }
+}
---
+++ FitnessTogetherTests/RegistrationStates/MockValidator.swift
@@ -0,0 +1,41 @@
+
+import Foundation
+import XCTest
+@testable import FitnessTogether
+
+internal class MockValidator: Validator {
+    
+    var isValidFirstName = true
+    var isValidLastName = true
+    var isValidDateOfBirth = true
+    var isValidEmail = true
+    var isValidPassword = true
+    var isValidConfirmPassword = true
+    var errorMessage: String? = "error"
+    
+    func isValidFirstName(_ string: String?) -> ValidatorResult {
+        return isValidFirstName ? .valid : .invalid(message: errorMessage)
+        
+    }
+    
+    func isValidLastName(_ string: String?) -> ValidatorResult {
+        return isValidLastName ? .valid : .invalid(message: errorMessage)
+    }
+    
+    func isValidDateOfBirth(_ string: Date?) -> ValidatorResult {
+        return isValidDateOfBirth ? .valid : .invalid(message: errorMessage)
+    }
+    
+    func isValidEmail(_ string: String?) -> ValidatorResult {
+        return isValidEmail ? .valid : .invalid(message: errorMessage)
+    }
+    
+    func isValidPassword(_ string: String?) -> ValidatorResult {
+        return isValidPassword ? .valid : .invalid(message: errorMessage)
+    }
+    
+    func isValidPasswordConfirmation(_ password: String?, _ confirmation: String?) -> ValidatorResult {
+        return isValidConfirmPassword ? .valid : .invalid(message: errorMessage)
+    }
+    
+}
