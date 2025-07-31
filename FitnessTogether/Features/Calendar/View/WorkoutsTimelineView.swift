803adb54a55a060784cacaff12d3e05fe1b95f31
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2025-07-31 16:05:58 +0300

    WorkoutsTimelineView
    Вью чтобы соединить таймлайн и тренировки

--- FitnessTogether/Features/Calendar/View/WorkoutsTimelineView.swift
+++ FitnessTogether/Features/Calendar/View/WorkoutsTimelineView.swift
@@ -3,8 +3,10 @@ import UIKit
 
 public class WorkoutsTimelineView: UIView {
     
-    var timelineView = TimeLineView()
-    let workoutsParentView = UIView()
+    var items: [WorkoutTimelineItem] = [] { didSet { updateItems() } }
+    
+    private let timelineView = TimeLineView()
+    private let workoutsParentView = UIView()
     
     //MARK: - Lifecycle
     public convenience init(){
@@ -23,12 +25,15 @@ public class WorkoutsTimelineView: UIView {
         isUserInteractionEnabled = true
         setupTimeLineView()
         backgroundColor = .systemBlue
+        
+        self.items = [
+            WorkoutTimelineItem(title: "Прохор нуб", color: .systemMint, column: 2, start: 9*3600, duration: 3*3600)
+        ]
     }
     
     public override func layoutSubviews() {
         super.layoutSubviews()
         constraintHeight(timelineView.intrinsicContentSize.height)
-        
     }
 
     private func setupTimeLineView() {
@@ -40,41 +45,110 @@ public class WorkoutsTimelineView: UIView {
         
         timelineView.backgroundColor = .systemBackground
         timelineView.tintColor = .systemGray3
-        addView(start: 3600*1, duration: 4*3600)
-        addView(start: 3600*17, duration: 5*3600)
-        addView(start: 3600*12 , duration: 3600)
         
         addSubview(workoutsParentView)
         workoutsParentView.isUserInteractionEnabled = false
         workoutsParentView.snp.makeConstraints { $0.edges.equalTo(timelineView.scheduleLayoutGuide) }
     }
     
-    private func addView(start: Int, duration: Int) {
-        let v = UIView()
-        v.isUserInteractionEnabled = false
-        v.backgroundColor = .systemBlue
-        
-        workoutsParentView.addSubview(v)
-        
-        let perDay = (24 * 60 * 60).cgf
-        let end = start + duration
-        let endOffset = end.cgf / perDay
-        let durationOffset = duration.cgf / perDay
-        
-        v.snp.makeConstraints { maker in
-            maker.width.equalToSuperview().dividedBy(timelineView.columnCount)
-            maker.leading.equalToSuperview()
-            maker.height.equalToSuperview().multipliedBy(durationOffset)
-            maker.bottom.equalToSuperview().multipliedBy(endOffset)
+    private func updateItems() {
+        for item in items {
+            let perDay = (24 * 60 * 60).cgf
+            
+            let itemView = WorkoutTimelineItemView()
+            itemView.item = item
+            let end = item.start + item.duration
+            let endOffset = end / perDay
+            let durationOffset = item.duration / perDay
+            
+            workoutsParentView.addSubview(itemView)
+            itemView.snp.makeConstraints { maker in
+                maker.leading.equalToSuperview()
+                maker.width.equalToSuperview().dividedBy(timelineView.columnCount)
+                maker.height.equalToSuperview().multipliedBy(durationOffset)
+                maker.bottom.equalToSuperview().multipliedBy(endOffset)
+            }
         }
     }
     
 }
 
 public struct WorkoutTimelineItem {
-    public
+    public var title: String
+    public var color: UIColor
+    public var column: Int
+    public var start: TimeInterval
+    public var duration: TimeInterval
 }
 
-fileprivate class WorkoutTimelineItemView {
+fileprivate class WorkoutTimelineItemView: UIView {
+    
+    var item: WorkoutTimelineItem?
+    var insets = UIEdgeInsets(top: 0, left: 0, bottom: 4, right: 4)
+    var font = DC.Font.roboto(weight: .medium, size: 12)
+    var cornerRadius: CGFloat = 4
+    
+    private var label = UILabel()
+    
+    //MARK: - Lifecycle
+    public convenience init(){
+        self.init(frame: .zero)
+    }
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setup() {
+        setupLabel()
+    }
+    
+    private func setupLabel() {
+        addSubview(label)
+        label.snp.makeConstraints { maker in
+            maker.top.leading.trailing.equalToSuperview().inset(4)
+            maker.bottom.equalToSuperview().priority(.medium)
+        }
+        label.snp.contentHuggingVerticalPriority = 1000
+        
+        label.textColor = .systemBackground
+        label.numberOfLines = 0
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        backgroundColor = .clear
+        label.attributedText = getLabelTitle()
+        setNeedsDisplay()
+    }
+    
+    private func getLabelTitle() -> NSAttributedString {
+        let paragraph = NSMutableParagraphStyle()
+        paragraph.lineBreakMode = .byWordWrapping
+        paragraph.hyphenationFactor = 0.9
+        
+        let nsAttributedString = NSAttributedString(string: item?.title ?? "", attributes: [
+            .paragraphStyle: paragraph,
+            .font: font,
+            .foregroundColor: UIColor.systemBackground
+        ])
+        
+        return nsAttributedString
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        let rectangle = CGRect(x: rect.minX + insets.left,
+                               y: rect.minY + insets.top,
+                               width: rect.width - insets.left - insets.right,
+                               height: rect.height - insets.top - insets.bottom)
+        let path = UIBezierPath(roundedRect: rectangle, cornerRadius: cornerRadius)
+        item?.color.setFill()
+        path.fill()
+    }
     
 }
